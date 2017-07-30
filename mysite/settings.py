1e5a5af7411906c53f61dad09daf39d5748b61fb
Beast102 <30573994+Beast102@users.noreply.github.com>
2017-07-30 22:05:27 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=0@!dwc3srfdy&p1%@pw^pzcja()6qlznk!-^kaef#b(qmru81'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [mig131.pythonanywhere.com]
 
 
 # Application definition
